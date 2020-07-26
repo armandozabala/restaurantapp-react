3d53b5ab79108086b4d4aef7247a636ef55ce01b
Armando <armandozabala@gmail.com>
2020-07-26 17:29:42 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router';
+
+import Ordenes from './components/pages/Ordenes'
+import Menu from "./components/pages/Menu";
+import NuevoPlato from "./components/pages/NuevoPlato";
+import Sidebar from './components/ui/Sidebar'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="md:flex min-h-screen">
+     
+      <Sidebar />
+
+      <div className="md:w-3/5 xl:w-4/5 p-6">
+        <Routes>
+          <Route path="/" element={<Ordenes />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/nuevo-plato" element={<NuevoPlato />} />
+        </Routes>
+      </div>
     </div>
   );
 }
---
+++ src/components/ui/Sidebar.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+
+const Sidebar = () => {
+    return (
+      <div className="md:w-2/5 xl:w-1/5 bg-gray-800">
+        <div className="p-6">
+          <p className="uppercase text-white text-2xl text-center font-bold">
+            {" "}
+            Restaurant App
+          </p>
+          <p className="mt-3 text-gray-600">
+            Administra tu Restaurant en las siguientes opciones
+          </p>
+          <nav className="mt-10">
+            <NavLink
+              className="p-1 text-gray-400 block hover:bg-yellow-500 hover:text-gray-900"
+              activeClassName="text-yellow-500"
+              exact
+              to="/"
+            >
+              Ordenes
+            </NavLink>
+            <NavLink
+              className="p-1 text-gray-400 block hover:bg-yellow-500 hover:text-gray-900"
+              activeClassName="text-yellow-500"
+              exact
+              to="/menu"
+            >
+              Menu
+            </NavLink>
+          </nav>
+        </div>
+      </div>
+    );
+}
+
+export default Sidebar;
